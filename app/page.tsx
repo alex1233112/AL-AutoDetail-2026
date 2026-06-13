02dfcb633b544f049e1c4b96ea412fbf863df417
alex1233112 <alexlec3233@gmail.com>
2026-06-12 21:59:36 -0400

    mise a jour du site

--- app/page.tsx
+++ app/page.tsx
@@ -246,7 +246,7 @@ export default function Home() {
 
           <div className="overflow-hidden rounded-[2rem] border border-white/10">
             <img
-              src="https://share.google/cCwqxepT4b2rFrlFhttps://share.google/Yy5BraukP7LZpwALz"
+              src="https://images.unsplash.com/photo-1607860108855-64acf2078ed9?q=80&w=1600&auto=format&fit=crop"
               alt="Polissage automobile"
               className="h-[550px] w-full object-cover"
             />
